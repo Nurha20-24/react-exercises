eb896a37110ec9b6614bc1fafd64eea5ea4e4554
nurhana <nur.hana@metropolia.fi>
2025-11-20 11:21:52 +0200

    Lab assignment 2 completed

--- webdev-react/src/views/Home.jsx
+++ webdev-react/src/views/Home.jsx
@@ -1,20 +1,10 @@
 import MediaRow from '../components/MediaRow';
 //import SingleView from '../components/SingleView';
 import {useState, useEffect} from 'react';
+import fetchData from '../utils/fetchData';
 
-const fetchData = async (url, options = {}) => {
-  // console.log('fetching data from url: ', url);
-  const response = await fetch(url, options);
-  const json = await response.json();
-  if (!response.ok) {
-    // console.log('json', json);
-    if (json.message) {
-      throw new Error(json.message);
-    }
-    throw new Error(`Error ${response.status} occured`);
-  }
-  return json;
-};
+const MEDIA_API = import.meta.env.VITE_MEDIA_API + '/media';
+const AUTH_API = import.meta.env.VITE_AUTH_API;
 
 const Home = () => {
   //const [selectedItem, setSelectedItem] = useState(null);
@@ -23,8 +13,36 @@ const Home = () => {
   useEffect(() => {
     const getMedia = async () => {
       try {
-        const data = await fetchData('/data.json');
-        setMediaArray(data);
+        //const data = await fetchData('/data.json');
+        //setMediaArray(data);
+
+        const mediaData = await fetchData(MEDIA_API);
+        //console.log('Fetched media data: ', mediaData);
+
+        const newArray = await Promise.all(
+          mediaData.map(async (item) => {
+            try {
+              const user = await fetchData(AUTH_API + '/users/' + item.user_id);
+              //console.log('Fetched user data: ', user.username);
+              return {
+                ...item,
+                username: user.username,
+              };
+            } catch (error) {
+              console.error(
+                'Error fetching user data for user_id',
+                item.user_id,
+                error
+              );
+              return {
+                ...item,
+                username: 'Unknown',
+              };
+            }
+          })
+        );
+        setMediaArray(newArray);
+        console.log('Media with users: ', newArray);
       } catch (error) {
         console.error('Error fetching media data: ', error);
       }
@@ -46,6 +64,7 @@ const Home = () => {
             <th>Created</th>
             <th>Size</th>
             <th>Type</th>
+            <th>Username</th>
             <th>View</th>
           </tr>
         </thead>
